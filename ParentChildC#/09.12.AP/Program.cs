2ac342909c8c6f99f998bbc78c7ed0b5b497cacb
Chunko <74864261+Lu4nd4@users.noreply.github.com>
2022-06-18 11:40:56 +0200

    Add files via upload

---
+++ ParentChildC#/09.12.AP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ namespace Project1
+{
+    class ParentClass
+    {
+        static void Main (string[] args)
+        {
+            //We are calling the parent classes and assigning them onto an object
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+            Animal myDog = new Dog();
+
+            //we are calling the child classes throught the objects we created (in this case there are no parameters in the child classes)
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+
+        //Parent class
+        class Animal
+        {
+            //Child class
+            public virtual void animalSound()
+            {
+               Console.WriteLine("Animal makes sound");
+            }
+        }
+        //Overriding the child class
+        class Dog : Animal
+        {
+            public override void animalSound()
+            {
+                Console.WriteLine("Dog makes sound");
+            }
+        }
+        //Also overriding child class
+        class Pig: Animal
+        {
+            public override void animalSound()
+            {
+                 Console.WriteLine("Pig makes sound");
+            }
+        }
+
+
+    }
+}
